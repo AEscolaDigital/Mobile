aeb2866000e9c139b24a99e49eac5725f32924a9
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-11-03 11:21:50 -0300

    implementando fragmentação

--- app/src/main/java/com/example/school/TeamsActivity.kt
+++ app/src/main/java/com/example/school/TeamsActivity.kt
@@ -2,14 +2,45 @@ package com.example.school
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.school.fragments.TeamsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TeamsActivity : AppCompatActivity() {
+
+    lateinit var bottom_navigation : BottomNavigationView
+    private val teamsFragment = TeamsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teams)
+        replaceFragment(teamsFragment)
 
+
+    bottom_navigation = findViewById(R.id.navegation_bottom)
+
+        bottom_navigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> replaceFragment(teamsFragment)
+                R.id.ic_tarefas -> replaceFragment(teamsFragment)
+            }
+
+            true
+        }
 
         // Remover a AppBar
         supportActionBar!!.hide()
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment != null){
+
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
+
+
