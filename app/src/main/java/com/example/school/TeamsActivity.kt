ea59c204dd2cb43566f141de30a6c02ecc0f1a53
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-11-04 11:21:41 -0300

    finalizando a fragmentação

--- app/src/main/java/com/example/school/TeamsActivity.kt
+++ app/src/main/java/com/example/school/TeamsActivity.kt
@@ -3,33 +3,72 @@ package com.example.school
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.example.school.fragments.TeamsFragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.school.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TeamsActivity : AppCompatActivity() {
 
     lateinit var bottom_navigation : BottomNavigationView
+    lateinit var reciclerTurmas : RecyclerView
+
     private val teamsFragment = TeamsFragment()
+    private val taskFragment = TaskFragment()
+    private val memberFragment = MemberFragment()
+    private val callFragment = CallFragment()
+    private val presencaFragment = PresencaFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teams)
         replaceFragment(teamsFragment)
 
+        // Remover a AppBar
+        supportActionBar!!.hide()
+
 
     bottom_navigation = findViewById(R.id.navegation_bottom)
 
-        bottom_navigation.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.ic_home -> replaceFragment(teamsFragment)
-                R.id.ic_tarefas -> replaceFragment(teamsFragment)
-            }
+        // Fazendo a troca de tela ao clicar em um icone no bottom_navigation
 
+        bottom_navigation.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.ic_home -> {
+                    replaceFragment(teamsFragment)
+                    true
+                }
+                R.id.ic_tarefas -> {
+                    replaceFragment(taskFragment)
+                    true
+                }
+                R.id.ic_addMembros -> {
+                    replaceFragment(memberFragment)
+                    true
+                }
+
+                R.id.ic_chamada -> {
+                    replaceFragment(callFragment)
+                    true
+                }
+
+                R.id.ic_presenca -> {
+                    replaceFragment(presencaFragment)
+                    true
+                }
+
+            }
             true
         }
 
-        // Remover a AppBar
-        supportActionBar!!.hide()
+        // Configuração da RecyclerView
+        //reciclerTurmas = findViewById(R.id.rv_teams)
+
+        // * Determinar o layout da RV(RecycleView)
+        //reciclerTurmas.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        //reciclerTurmas.layoutManager = GridLayoutManager(this, 2 )
 
     }
 
