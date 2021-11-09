35e030e2adb5d6eff030533aa0e193cf4e7b3d33
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-11-09 11:26:36 -0300

    iniciando o modal de disciplina

--- app/src/main/java/com/example/school/activities/TeamsActivity.kt
+++ app/src/main/java/com/example/school/activities/TeamsActivity.kt
@@ -2,13 +2,36 @@ package com.example.school.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.example.school.R
+
+
+lateinit var btn_criar_disciplina: Button
+
 
 class TeamsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teams)
 
+
+        btn_criar_disciplina = findViewById(R.id.btn_criar_disciplina)
+
+
+        btn_criar_disciplina.setOnClickListener{
+
+           val view = View.inflate(this, R.layout.dialog_displine, null)
+
+            val builder = AlertDialog.Builder(this)
+            builder.setView(view)
+
+            val dialog = builder.create()
+            dialog.show()
+
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        }
 
         // Remover a AppBar
         supportActionBar!!.hide()
