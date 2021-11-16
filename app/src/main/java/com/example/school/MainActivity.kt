41dfe5671e765cac008f937010063edccfb5abb5
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-11-16 11:26:39 -0300

    commit com erros

--- app/src/main/java/com/example/school/MainActivity.kt
+++ app/src/main/java/com/example/school/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.school
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navigationView: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        navigationView = findViewById(R.id.navigationView)
+
     }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/school/fragments/TaskFragment.kt
+++ app/src/main/java/com/example/school/fragments/TaskFragment.kt
@@ -1,10 +1,8 @@
 package com.example.school.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.school.R
 
 // TODO: Rename parameter arguments, choose names that match
