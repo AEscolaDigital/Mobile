3c39c5df5e96c882f4001211725624bbf44f510a
AntonioAiltonGocalves <antonioailton723@gmail.com>
2021-11-19 08:12:48 -0300

    list discipline

--- app/src/main/java/com/example/school/api/school/ApiSchool.kt
+++ app/src/main/java/com/example/school/api/school/ApiSchool.kt
@@ -1,12 +1,11 @@
 package com.example.school.api.school
 
+import com.example.school.models.Discipline
 import com.example.school.models.School
-import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 
 class ApiSchool {
@@ -17,12 +16,17 @@ class ApiSchool {
     }
 
     interface Sessions{
-        @POST("sessions")
+        @POST("/sessions")
         fun login(@Body body: com.example.school.models.Login): Call<com.example.school.models.Login>
     }
 
+    interface Dashboad{
+        @GET("/disciplines")
+        fun listDisciplines(@Header("Authorization") token: String): Call<List<Discipline>>
+    }
+
     class SchoolEndPoint {
-        val url = "http://10.0.0.100:3333/"
+        val url = "http://10.0.0.102:3333/"
         val service = Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build()
 
         fun getService(): ApiService {
@@ -32,5 +36,10 @@ class ApiSchool {
         fun sessionsService(): Sessions{
             return service.create(Sessions::class.java)
         }
+
+        fun dashboardService():Dashboad{
+            return service.create(Dashboad::class.java)
+        }
+
     }
 }
--- app/src/main/java/com/example/school/fragments/TeamsFragment.kt
+++ app/src/main/java/com/example/school/fragments/TeamsFragment.kt
@@ -1,19 +1,24 @@
 package com.example.school.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.school.R
 import com.example.school.adapter.DashboardAdapter
+import com.example.school.api.school.ApiSchool
+import com.example.school.models.Discipline
+import com.example.school.models.Login
+import com.example.school.models.UserLogiResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TeamsFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
     private lateinit var recyclerViewTurmas :RecyclerView
     private lateinit var dashBoardAdapter: DashboardAdapter
 
@@ -38,15 +43,60 @@ class TeamsFragment : Fragment() {
         recyclerViewTurmas = view.findViewById(R.id.recycler_teams)!!
         dashBoardAdapter = DashboardAdapter(context)
 
-        // Determinar o layout da RV(RecycleView)
-        /*recyclerViewTurmas.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)*/
-
+        //layout of recyclerview, grid two columns
         recyclerViewTurmas.layoutManager = GridLayoutManager(context, 2)
 
         //* Definindo a Adapter da RV(RecycleView)
         recyclerViewTurmas.adapter = dashBoardAdapter
-    }
 
+        //call the shared preferences school and get jwt
+        val sharedPreferences = context.getSharedPreferences("school", 0)
+        val jwt = sharedPreferences.getString("JWT", "")
+
+        Log.i("REQUEST", jwt!!)
+
+
+        //call the api for populate disciplines of studant
+        val remote = ApiSchool.SchoolEndPoint().dashboardService()
+        val call: Call<List<Discipline>> = remote.listDisciplines(jwt!!)
+
+        //aply a request async and get the response
+        call.enqueue(object : Callback<List<Discipline>> {
+            override fun onResponse(call: Call<List<Discipline>>, response: Response<List<Discipline>>) {
+                Log.i("RESPONSE", response.body().toString())
+                Log.i("RESPONSE", response.message().toString())
+                Log.i("RESPONSE", response.code().toString())
+                Log.i("RESPONSE", response.errorBody().toString())
+                Log.i("RESPONSE", response.isSuccessful.toString())
+                Log.i("RESPONSE", response.headers().toString())
+                Log.i("RESPONSE", response.raw().toString())
+            }
+
+            override fun onFailure(call: Call<List<Discipline>>, t: Throwable) {
+                Log.i("REQUEST", "FAIL")
+            }
+        })
+/*
+        call.enqueue(object : Callback<List<Discipline>> {
+            override fun onResponse(call: Call<List<Discipline>>, response: Response<List<Discipline>>) {
+                /*dashBoardAdapter.updateListasDisciplina(response.body()!!)*/
+                Log.i("RESPONSE", response.body().toString())
+                Log.i("RESPONSE", response.message().toString())
+                Log.i("RESPONSE", response.code().toString())
+                Log.i("RESPONSE", response.errorBody().toString())
+                Log.i("RESPONSE", response.isSuccessful.toString())
+                Log.i("RESPONSE", response.headers().toString())
+                Log.i("RESPONSE", response.raw().toString())
+
+            }
+
+            override fun onFailure(call: Call<List<Discipline>>, t: Throwable) {
+                Log.e("onFailure", t.message.toString())
+            }
+
+        })
+*/
+
+    }
 
 }
